7d1555cf1ca715d717a9569885a29d4062f59a61
Stephen <33720231+Bas15@users.noreply.github.com>
2025-03-24 00:13:09 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+    build: {
+    outDir: "src/dist", // Specify the output directory
+    emptyOutDir: true, // Clears the output directory before building
+  },
 });
